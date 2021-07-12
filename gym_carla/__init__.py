1ba4cb0298c2907188a8e50f50e2be153edc395b
rudyn2 <c01010010@hotmail.com>
2021-07-12 13:53:49 -0400

    now the distribution package is well defined

---
+++ gym_carla/__init__.py
@@ -0,0 +1,1 @@
+from gym_carla.envs.carla_env import CarlaEnv
--- setup.py
+++ setup.py
@@ -1,6 +1,15 @@
-from setuptools import setup
+from setuptools import setup, find_packages
+import pathlib
+import os
+
+root = pathlib.Path(__file__).parent
+os.chdir(str(root))
 
 setup(name='gym_carla',
-      version='0.0.2',
-      install_requires=['gym', 'pygame'] 
+      version='0.1.0',
+      author='Rudy Garcia A.',
+      author_email='c01010010@hotmail.com',
+      license='MIT',
+      packages=find_packages(),
+      install_requires=['gym', 'pygame']
 )
